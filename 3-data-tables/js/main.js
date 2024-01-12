d2d699146f3787f3e3c4f183dd8a70c631c918ed
Bohdan Bilokin <bodya24102001@gmail.com>
2024-01-12 17:38:52 +0200

    Fixed arrows display depends on sorting, fixed an issue with number definitoins after sorting

--- 3-data-tables/js/main.js
+++ 3-data-tables/js/main.js
@@ -3,25 +3,22 @@ function DataTable(config, data) {
   const tableDiv = document.querySelector(config.parent);
   const table = document.createElement("table");
   tableDiv.appendChild(table);
-  table.style.width = "100%";  
-  table.style.height = "auto";
-  table.style.margin = "50px 0";
   table.setAttribute("class","table");
-  table.setAttribute("border","1");
+  const initialId = getFirstId(data);
   
   createTitleRow(table, config);
-  createMainRows(table, config, data);
-  sortAll(table, users, config);
+  createMainRows(table, config, data, initialId);
+  setupSortingEvents(table, users, config, initialId);
 }
  
 const config1 = {
-   parent: '#usersTable',
-   columns: [
-     {title: 'Ім’я', value: 'name'},
-     {title: 'Прізвище', value: 'surname'},
-     {title: 'Вік', value: 'age'},
-    ]
-  };
+  parent: '#usersTable',
+  columns: [
+    {title: 'Ім’я', value: 'name'},
+    {title: 'Прізвище', value: 'surname'},
+    {title: 'Вік', value: 'age'},
+  ]
+};
  
 const users = [
   {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
@@ -29,7 +26,23 @@ const users = [
   {id: 30052, name: 'Ігор', surname: 'Ігоренко', age: 25},
   {id: 30053, name: 'Ольга', surname: 'Олійник', age: 30},
   {id: 30054, name: 'Михайло', surname: 'Михайлик', age: 18},
+  {id: 30055, name: 'Ірина', surname: 'Іванова', age: 22},
+  {id: 30056, name: 'Петро', surname: 'Петренко', age: 35},
+  {id: 30057, name: 'Оксана', surname: 'Оксенчук', age: 28},
+  {id: 30058, name: 'Андрій', surname: 'Андрійчук', age: 19},
+  {id: 30059, name: 'Тетяна', surname: 'Тетянчук', age: 40},
+  {id: 30060, name: 'Сергій', surname: 'Сергієнко', age: 32},
+  {id: 30061, name: 'Наталія', surname: 'Наталюк', age: 29},
+  {id: 30062, name: 'Дмитро', surname: 'Дмитренко', age: 24},
+  {id: 30063, name: 'Людмила', surname: 'Людвіга', age: 27},
+  {id: 30064, name: 'Максим', surname: 'Максимович', age: 31},
+  {id: 30065, name: 'Галина', surname: 'Галенко', age: 23},
+  {id: 30066, name: 'Євген', surname: 'Євгенчук', age: 26},
+  {id: 30067, name: 'Катерина', surname: 'Катеринчук', age: 33},
+  {id: 30068, name: 'Роман', surname: 'Романович', age: 17},
+  {id: 30069, name: 'Анна', surname: 'Анненко', age: 20}
 ];
+
 
 DataTable(config1, users);
 DataTable(config1, users);
@@ -37,17 +50,16 @@ DataTable(config1, users);
 function createTitleRow(table, config) {
   let thead = document.createElement("thead");
   table.appendChild(thead);
+  const titleRow = thead.insertRow(0);
 
   // Add symbol "№"
-  const titleRow = thead.insertRow(0);
   const numberSymbolTh = document.createElement("th");
   const numberSymbolNode = document.createTextNode("№");
   numberSymbolTh.appendChild(numberSymbolNode);
   titleRow.appendChild(numberSymbolTh);
   numberSymbolTh.classList.add("id");
-  let arrowUp = document.createElement("img");
-  arrowUp.setAttribute("src","images/arrow-up.png");
-  numberSymbolTh.appendChild(arrowUp);
+
+  addArrowImg(numberSymbolTh);
 
   for(let i = 1; i <= config.columns.length; i++){
     const th = document.createElement("th");
@@ -56,13 +68,21 @@ function createTitleRow(table, config) {
     th.classList.add(config.columns[i - 1].value);
     titleRow.appendChild(th);
 
-    arrowUp = document.createElement("img");
-    arrowUp.setAttribute("src","images/arrow-up.png");
-    th.appendChild(arrowUp);
+    addArrowImg(th);
   }  
 }
 
-function createMainRows(table, config, data) {
+function getFirstId(data) {
+  return data[0].id;
+}
+
+function addArrowImg(cell) {
+  let arrowUp = document.createElement("img");
+  arrowUp.setAttribute("src","images/arrow-up.png");
+  cell.appendChild(arrowUp);
+}
+
+function createMainRows(table, config, data, initialId) { // Create rows for table body
   let tbody = document.createElement("tbody");
   table.appendChild(tbody);
 
@@ -71,24 +91,24 @@ function createMainRows(table, config, data) {
     const tableRow = tbody.insertRow(i);
 
     const numberTd = document.createElement("td");
-    const numberTdNode = document.createTextNode((data[i].id % 10) + 1); // Reminder of id
+    const numberTdNode = document.createTextNode((data[i].id - initialId) + 1 ); // Reminder of id
+    console.log(initialId + " inId");
     numberTd.appendChild(numberTdNode);
     tableRow.appendChild(numberTd);
-    numberTd.classList.add("id");
 
     // Create sells
     for(let j = 1; j <= config.columns.length; j++){
       const td = document.createElement("td");
       const columnValue = config.columns[j - 1].value; // get value from config to use them like a key while creating node
       const node = document.createTextNode(data[i][columnValue]);
-      td.classList.add(columnValue); // aaa
       td.appendChild(node);
       tableRow.appendChild(td);
     }   
   }
 }
 
-function sortAll(table, data, config){
+// Providing table data sorting
+function setupSortingEvents(table, data, config, initialId){
   const titleCells = table.querySelectorAll("th");
   for(let i = 0; i < titleCells.length; i++){
     titleCells[i].addEventListener("click", sort.bind(titleCells[i]));
@@ -96,25 +116,38 @@ function sortAll(table, data, config){
   
   function sort() {
     table.querySelector("tbody").remove();
-    const dataCopy = JSON.stringify(data);
-    const image = table.querySelector("img");
+    const image = this.querySelector("img");
     let valueToCompare = this.className;
 
     if(valueToCompare === "age" || valueToCompare === "id"){
-      data.sort((a, b) => a[valueToCompare] - b[valueToCompare]);
-      if(JSON.stringify(data) === dataCopy){ // Make reverse sort if it's already sorted
-        data.sort((a, b) => b[valueToCompare] - a[valueToCompare]);
-        image.setAttribute("src","images/arrow-down.png")  
-      }
+      sortNumbers(data, image, valueToCompare);
     } else {
-      data.sort((a, b) => a[valueToCompare].localeCompare(b[valueToCompare]));
-      if(JSON.stringify(data) === dataCopy){ // Make reverse sort if it's already sorted
-        data.sort((a, b) => b[valueToCompare].localeCompare(a[valueToCompare]));
-        image.setAttribute("src","images/arrow-down.png")  
-      }
+      sortStrings(data, image, valueToCompare);
     }
     
     console.log(data);
-    createMainRows(table, config, data);
+    createMainRows(table, config, data, initialId); // Rewrite body of the table
+  }
+}
+
+function sortNumbers(data, image, valueToCompare) {
+  const dataCopy = JSON.stringify(data); // Copy of an array for further comparison  
+  data.sort((a, b) => a[valueToCompare] - b[valueToCompare]);
+  image.setAttribute("src","images/arrow-up.png");
+
+  if(JSON.stringify(data) === dataCopy){ // Make reverse sort if it's already sorted
+    data.sort((a, b) => b[valueToCompare] - a[valueToCompare]);
+    image.setAttribute("src","images/arrow-down.png"); 
+  }
+}
+
+function sortStrings(data, image, valueToCompare) {
+  const dataCopy = JSON.stringify(data); // Copy of an array for further comparison 
+  data.sort((a, b) => a[valueToCompare].localeCompare(b[valueToCompare]));
+  image.setAttribute("src","images/arrow-up.png");
+
+  if(JSON.stringify(data) === dataCopy){ // Make reverse sort if it's already sorted
+    data.sort((a, b) => b[valueToCompare].localeCompare(a[valueToCompare]));
+    image.setAttribute("src","images/arrow-down.png");  
   }
 }
